0d645e3c2a120d85a8d6c9fb49d3e75800ceff83
bunsung92 <bunsung92@naver.com>
2022-07-05 17:14:04 +0900

    init: github-action [#1]

---
+++ jenkins-pr/src/main/java/com/example/jenkinspr/JenkinsPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPrApplication.class, args);
+    }
+
+}
---
+++ jenkins-pr/src/test/java/com/example/jenkinspr/JenkinsPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
