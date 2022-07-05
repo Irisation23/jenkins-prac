ad501bddd69205249e9147b345b0681384970793
bunsung92 <bunsung92@naver.com>
2022-07-05 17:50:19 +0900

    소나큐브 적용했으면 좀 되라

--- src/main/java/com/example/jenkinspr/dto/IphoneRequestDto.java
+++ src/main/java/com/example/jenkinspr/dto/IphoneRequestDto.java
@@ -3,4 +3,12 @@ package com.example.jenkinspr.dto;
 
 public class IphoneRequestDto {
     private Long battery;
+
+    public void setBattery(Long battery) {
+        this.battery = battery;
+    }
+
+    public Long getBattery() {
+        return battery;
+    }
 }
--- src/test/java/com/example/jenkinspr/JenkinsPrApplicationTests.java
+++ src/test/java/com/example/jenkinspr/JenkinsPrApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.jenkinspr;
 
+import com.example.jenkinspr.dto.IphoneRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsPrApplicationTests {
 
+
     @Test
     void contextLoads() {
+
+        IphoneRequestDto iphoneRequestDto = new IphoneRequestDto();
+
+        iphoneRequestDto.setBattery(90L);
+
+        Long battery = iphoneRequestDto.getBattery();
+
+        if(battery.equals(90L)) {
+            System.out.println("굿");
+        }
+
     }
 
 }
